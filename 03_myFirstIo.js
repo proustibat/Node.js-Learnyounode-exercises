54bdb200785560a379497aa8ae8495a7c2479566
Jennifer Proust <jennifer.proust@gmail.com>
2014-07-23 17:23:39 +0200

    Fichiers renommés

--- 03_myFirstIo.js
+++ 03_myFirstIo.js
@@ -6,7 +6,7 @@ try {
 	buf = fs.readFileSync(fileName, 'utf8');
 	var buffStr = buf.toString();
 	var linesArr = buffStr.split('\n');
-	nbLines = linesArr.length;
+	nbLines = linesArr.length -1; // -1 because of line in file's end
 	console.log(nbLines);
 }
 catch (e) {
